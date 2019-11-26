eecdc5bae8366aa59f88b7fd537dca6356ed262d
Alex <alexander_lizarraga@ymail.com>
2019-11-26 02:43:42 -0500

    package.json

--- server/connection.js
+++ server/connection.js
@@ -8,8 +8,8 @@ var Sequelize = require("sequelize");
 // Creates mySQL connection using Sequelize
 // var sequelize = new Sequelize("Chirp", "root", "23642134aa", {
 //     host: "localhost",
-var sequelize = new Sequelize("Loan", "root", "23642134aa", {
-    host: "localhost",
+var sequelize = new Sequelize("heroku_9ebd3a2e0bb7c13", "b0cf29109b9a6a", "3b1f3ac9", {
+    host: "us-cdbr-iron-east-05.cleardb.net",
     dialect: "mysql",
     pool: {
         max: 5,
